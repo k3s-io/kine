0796c1308155326225065d3b16f79416c305ef86
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2020-07-14 19:46:33 +0800

    move the schema into its own place too

--- pkg/drivers/alpha/gorm/gorm.go
+++ pkg/drivers/alpha/gorm/gorm.go
@@ -8,18 +8,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type KineEntry struct {
-	ID             int64  `gorm:"primaryKey;index:name_id;AUTO_INCREMENT"`
-	Name           string `gorm:"index;index:name_id;uniqueIndex:name_prev_revision"`
-	Created        bool
-	Deleted        bool
-	CreateRevision int64
-	PrevRevision   int64 `gorm:"uniqueIndex:name_prev_revision"`
-	Lease          int64
-	Value          []byte
-	OldValue       []byte
-}
-
 type GormBacked struct {
 	DB                   *gorm.DB
 	HandleInsertionError func(error) error
