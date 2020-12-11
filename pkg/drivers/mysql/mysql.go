6a9cd6195735c766ee0a5c15ea549733391918cc
yue9944882 <291271447@qq.com>
2020-12-11 18:34:31 +0800

    explicitly use ascii charset for the name column
    Signed-off-by: yue9944882 <291271447@qq.com>

--- pkg/drivers/mysql/mysql.go
+++ pkg/drivers/mysql/mysql.go
@@ -24,7 +24,7 @@ var (
 		`CREATE TABLE IF NOT EXISTS kine
 			(
 				id INTEGER AUTO_INCREMENT,
-				name VARCHAR(630),
+				name VARCHAR(630) CHARACTER SET ascii,
 				created INTEGER,
 				deleted INTEGER,
 				create_revision INTEGER,
