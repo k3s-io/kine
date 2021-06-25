707bb524033e6b6c565aa8458de49bc9ed2407bd
Brad Davidson <brad.davidson@rancher.com>
2021-06-25 15:54:54 -0700

    WIP
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -2,29 +2,44 @@ package client
 
 import (
 	"context"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/k3s-io/kine/pkg/endpoint"
 )
 
-var (
-	config = endpoint.ETCDConfig{Endpoints: []string{"localhost:2379"}}
-)
+func getClient(t *testing.T) (Client, error) {
+	e := endpoint.ETCDConfig{Endpoints: []string{"localhost:2379"}}
+	if str := os.Getenv("K3S_DATASTORE_ENDPOINT"); str != "" {
+		// Strip off the scheme, if present
+		parts := strings.SplitN(str, "://", 2)
+		if len(parts) > 1 {
+			str = parts[1]
+		}
+		e.Endpoints = strings.Split(str, ",")
+	}
+	t.Logf("Connecting to etcd at %v", e.Endpoints)
+	return New(e)
+}
 
 func TestList(t *testing.T) {
-	c, err := New(config)
+	c, err := getClient(t)
 	if err != nil {
 		t.Errorf("Unable to create new client: %v", err)
 	}
 
-	_, err = c.List(context.TODO(), "/bootstrap", 0)
+	values, err := c.List(context.TODO(), "/bootstrap", 0)
 	if err != nil {
 		t.Errorf("Failed to list /bootstrap: %v", err)
+	}
+	if len(values) != 0 {
+		t.Error("Expected 0 values in list response")
 	}
 }
 
 func TestCreateAndList(t *testing.T) {
-	c, err := New(config)
+	c, err := getClient(t)
 	if err != nil {
 		t.Errorf("Unable to create new client: %v", err)
 	}
@@ -34,6 +49,10 @@ func TestCreateAndList(t *testing.T) {
 	values, err := c.List(context.TODO(), "/bootstrap", 0)
 	if err != nil {
 		t.Errorf("Failed to list /bootstrap: %v", err)
+	}
+
+	if len(values) != 1 {
+		t.Errorf("Expected 1 value in list response")
 	}
 
 	for _, v := range values {
