4d0191f7bf5e53bc54e4657981f6dac601de6195
yuanyanran <yuanyanran@megvii.com>
2022-07-26 11:04:16 +0800

    support get op: get op support: order by key and specific rangeEnd key

--- pkg/server/list.go
+++ pkg/server/list.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"sort"
 	"strings"
 
 	"go.etcd.io/etcd/api/v3/etcdserverpb"
@@ -14,11 +15,13 @@ func (l *LimitedServer) list(ctx context.Context, r *etcdserverpb.RangeRequest) 
 		return nil, fmt.Errorf("invalid range end length of 0")
 	}
 
-	prefix := string(append(r.RangeEnd[:len(r.RangeEnd)-1], r.RangeEnd[len(r.RangeEnd)-1]-1))
+	format_range_end := string(append(r.RangeEnd[:len(r.RangeEnd)-1], r.RangeEnd[len(r.RangeEnd)-1]-1))
+	start := string(bytes.TrimRight(r.Key, "\x00"))
+
+	prefix := start
 	if !strings.HasSuffix(prefix, "/") {
 		prefix = prefix + "/"
 	}
-	start := string(bytes.TrimRight(r.Key, "\x00"))
 
 	if r.CountOnly {
 		rev, count, err := l.backend.Count(ctx, prefix)
@@ -36,20 +39,40 @@ func (l *LimitedServer) list(ctx context.Context, r *etcdserverpb.RangeRequest) 
 		limit++
 	}
 
-	rev, kvs, err := l.backend.List(ctx, prefix, start, limit, r.Revision)
+	rev, kvs, err := l.backend.List(ctx, prefix, start, 0, r.Revision)
 	if err != nil {
 		return nil, err
 	}
 
+	sort.SliceStable(kvs, func(i, j int) bool {
+		return kvs[i].Key < kvs[j].Key
+	})
+	filteredKVS := make([]*KeyValue, 0, len(kvs))
+	if format_range_end != string(r.Key) {
+		for _, kv := range kvs {
+			if kv.Key <= format_range_end {
+				filteredKVS = append(filteredKVS, kv)
+			}
+		}
+	} else {
+		filteredKVS = kvs
+	}
+
+	if r.SortOrder == etcdserverpb.RangeRequest_DESCEND {
+		sort.SliceStable(filteredKVS, func(i, j int) bool {
+			return filteredKVS[i].Key > filteredKVS[j].Key
+		})
+	}
+
 	resp := &RangeResponse{
 		Header: txnHeader(rev),
-		Count:  int64(len(kvs)),
-		Kvs:    kvs,
+		Count:  int64(len(filteredKVS)),
+		Kvs:    filteredKVS,
 	}
 
 	if limit > 0 && resp.Count > r.Limit {
 		resp.More = true
-		resp.Kvs = kvs[0 : limit-1]
+		resp.Kvs = filteredKVS[0 : limit-1]
 	}
 
 	return resp, nil
