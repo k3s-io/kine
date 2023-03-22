ab647f3ebf6d5d81e21a6976f893349761215b0e
Adam Shannag <shannagadam11@gmail.com>
2023-03-22 21:56:36 +0800

    chore: mod update

--- pkg/endpoint/oracleCPC.go
+++ pkg/endpoint/oracleCPC.go
@@ -1,6 +1,6 @@
 package endpoint
 
-import "github.com/AdamShannag/kine/pkg/drivers/oracle"
+import "github.com/k3s-io/kine/pkg/drivers/oracle"
 
 func OracleConnectionPoolConfig(cfg Config) oracle.ConnectionPoolConfig {
 	return oracle.ConnectionPoolConfig{
