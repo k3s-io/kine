b5bab14f556301635d7dbf42064699e35e561f75
Ian Bishop <151477169+ianb-mp@users.noreply.github.com>
2025-02-20 13:24:47 +1000

    return url encoding hint with url parse error, use errors.Wrap()
    Signed-off-by: Ian Bishop <151477169+ianb-mp@users.noreply.github.com>

--- pkg/util/url.go
+++ pkg/util/url.go
@@ -1,15 +1,12 @@
 package util
 
 import (
-	"fmt"
 	"net/url"
+
+	"github.com/pkg/errors"
 )
 
 func ParseURL(urlStr string) (*url.URL, error) {
 	u, err := url.Parse(urlStr)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing URL. Ensure that any embedded values such as username & password have been URL encoded: %w", err)
-	}
-
-	return u, nil
+	return u, errors.Wrap(err, "error parsing URL. Ensure that any embedded values such as username & password have been URL encoded")
 }
