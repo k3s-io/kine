45e57d52a001e526c8be580b2c8d0b15c5624517
Brad Davidson <brad.davidson@rancher.com>
2025-12-11 05:38:18 +0000

    Summarize byte slices when logging SQL
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/util/string.go
+++ pkg/util/string.go
@@ -1,13 +1,35 @@
 package util
 
 import (
+	"fmt"
 	"regexp"
-	"strings"
 )
 
+var whitespace = regexp.MustCompile("[\n\t ]+")
+
+// Stripped wraps a string, stripping newlines and collapsing whitespace
 type Stripped string
 
 func (s Stripped) String() string {
-	str := strings.ReplaceAll(string(s), "\n", "")
-	return regexp.MustCompile("[\t ]+").ReplaceAllString(str, " ")
+	return whitespace.ReplaceAllString(string(s), " ")
+}
+
+// Summarize wraps a slice of any, and when stringed only prints the type/length of complex arguments
+type Summarize []any
+
+func (s Summarize) String() string {
+	ret := make([]any, len(s))
+	for i := range s {
+		switch v := s[i].(type) {
+		case int, uint, int8, uint8, int16, uint16, int32, uint32, int64, uint64, float32, float64, complex64, complex128, uintptr, bool:
+			ret[i] = v
+		case string:
+			ret[i] = fmt.Sprintf("'%s'", v)
+		case []byte:
+			ret[i] = fmt.Sprintf("[%d]byte(...)", len(v))
+		default:
+			ret[i] = fmt.Sprintf("%T{...}", v)
+		}
+	}
+	return fmt.Sprint(ret)
 }
