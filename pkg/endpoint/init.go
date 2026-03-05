5a3aa5f93b4639b0a60a7a797a138030635b7ecb
Brad Davidson <brad.davidson@rancher.com>
2026-03-05 06:46:24 +0000

    Add support for remote unix socket addresses
    K3s uses the endpoint package to parse the datastore-endpoint flag, which does
    not currently support connecting to standalone kine over a unix socket.
    
    This will allow kine to connect to etcd or kine on sockets, in addition
    to the existing http/https schemes.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/endpoint/init.go
+++ pkg/endpoint/init.go
@@ -2,9 +2,9 @@ package endpoint
 
 import (
 	// Import all the default drivers
-	_ "github.com/k3s-io/kine/pkg/drivers/http"
 	_ "github.com/k3s-io/kine/pkg/drivers/mysql"
 	_ "github.com/k3s-io/kine/pkg/drivers/nats"
 	_ "github.com/k3s-io/kine/pkg/drivers/pgsql"
+	_ "github.com/k3s-io/kine/pkg/drivers/remote"
 	_ "github.com/k3s-io/kine/pkg/drivers/sqlite"
 )
