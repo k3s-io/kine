7dbef0eb9ddef83b74dbb29194afcd743ab28132
Brad Davidson <brad.davidson@rancher.com>
2026-05-28 22:11:37 +0000

    Add support for pure-go sqlite
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- go.mod
+++ go.mod
@@ -27,6 +27,7 @@ require (
 	google.golang.org/grpc v1.81.1
 	k8s.io/apiserver v0.35.4
 	k8s.io/client-go v0.35.4
+	modernc.org/sqlite v1.51.0
 )
 
 require (
@@ -82,6 +83,7 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/minio/crc64nvme v1.1.1 // indirect
 	github.com/minio/highwayhash v1.0.4 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
@@ -92,12 +94,14 @@ require (
 	github.com/nats-io/jwt/v2 v2.8.1 // indirect
 	github.com/nats-io/nkeys v0.4.15 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/ncruces/go-strftime v1.0.0 // indirect
 	github.com/philhofer/fwd v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.67.5 // indirect
 	github.com/prometheus/procfs v0.20.1 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
@@ -147,6 +151,9 @@ require (
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20250910181357-589584f1c912 // indirect
 	k8s.io/utils v0.0.0-20251002143259-bc988d571ff4 // indirect
+	modernc.org/libc v1.72.3 // indirect
+	modernc.org/mathutil v1.7.1 // indirect
+	modernc.org/memory v1.11.0 // indirect
 	sigs.k8s.io/json v0.0.0-20250730193827-2d320260d730 // indirect
 	sigs.k8s.io/randfill v1.0.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v6 v6.3.0 // indirect
--- pkg/drivers/sqlite/sqlite_nocgo.go
+++ pkg/drivers/sqlite/sqlite_nocgo.go
@@ -3,31 +3,98 @@
 package sqlite
 
 import (
-	"context"
-	"database/sql"
-	"errors"
-	"sync"
+	"fmt"
+	"net/url"
+	"path/filepath"
+	"strings"
 
-	"github.com/k3s-io/kine/pkg/drivers"
-	"github.com/k3s-io/kine/pkg/drivers/generic"
+	"github.com/k3s-io/kine/pkg/query"
 	"github.com/k3s-io/kine/pkg/server"
+	sqlite3 "modernc.org/sqlite"
+	sqlite3lib "modernc.org/sqlite/lib"
 )
 
-var errNoCgo = errors.New("this binary is built without CGO, sqlite is disabled")
+func newConnector(driverName, dsn string) (*sqliteConnector, error) {
+	var err error
+	dsn, err = translateDSN(dsn)
+	if err != nil {
+		return nil, err
+	}
 
-func New(_ context.Context, _ *sync.WaitGroup, _ *drivers.Config) (bool, server.Backend, error) {
-	return false, nil, errNoCgo
+	driver := &sqlite3.Driver{}
+	if driverName == "litestream" {
+		driver.RegisterConnectionHook(func(conn sqlite3.ExecQuerierContext, dsn string) error {
+			var err error
+			if ctrl, ok := conn.(sqlite3.FileControl); ok {
+				_, err = ctrl.FileControlPersistWAL("main", 1)
+			}
+			return err
+		})
+	}
+	return &sqliteConnector{dsn: dsn, driver: driver}, nil
 }
 
-func NewVariant(_ context.Context, _ *sync.WaitGroup, _ string, cfg *drivers.Config) (server.Backend, *generic.Generic, error) {
-	return nil, nil, errNoCgo
+// translateDSN translates a subset of mattn/go-sqlite3 DSN parameters to modernc/sqlite _pragmas
+// only commonly used params are translated, others are passed through as-is
+func translateDSN(dsn string) (string, error) {
+	path, params, _ := strings.Cut(dsn, "?")
+	old, err := url.ParseQuery(params)
+	if err != nil {
+		return "", err
+	}
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return "", err
+	}
+	new := make(url.Values)
+	addAll := func(key, format string, vals []string) {
+		for _, val := range vals {
+			new.Add(key, fmt.Sprintf(format, val))
+		}
+	}
+	for key, vals := range old {
+		switch key {
+		case "_busy_timeout", "_timeout":
+			addAll("_pragma", "busy_timeout(%s)", vals)
+		case "_journal", "_journal_mode":
+			addAll("_pragma", "journal_mode(%s)", vals)
+		case "_synchronous", "_sync":
+			addAll("_pragma", "synchronous(%s)", vals)
+		case "cache":
+			// shared cache mode is not supported
+		default:
+			addAll(key, "%s", vals)
+		}
+	}
+	params, err = url.QueryUnescape(new.Encode())
+	if err != nil {
+		return "", err
+	}
+	return "file://" + path + "?" + params, nil
 }
 
-func setup(db *sql.DB) error {
-	return errNoCgo
+func version() string {
+	return fmt.Sprintf("modernc.org/sqlite version %s", sqlite3lib.SQLITE_VERSION)
 }
 
-func init() {
-	drivers.Register("sqlite", New)
-	drivers.SetDefault("sqlite")
+func postCompact() *query.Named {
+	// wal_checkpoint(FULL) hangs in the busy handler, an issue unique to this sqlite variant
+	return query.New(`PRAGMA wal_checkpoint(PASSIVE)`, "?", false, "PostCompactSQL")
+}
+
+func translateErr(err error) error {
+	if err, ok := err.(*sqlite3.Error); ok && err.Code() == sqlite3lib.SQLITE_CONSTRAINT_UNIQUE {
+		return server.ErrKeyExists
+	}
+	return err
+}
+
+func errCode(err error) string {
+	if err == nil {
+		return ""
+	}
+	if err, ok := err.(*sqlite3.Error); ok {
+		return sqlite3.ErrorCodeString[err.Code()]
+	}
+	return err.Error()
 }
