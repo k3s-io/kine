6a267d5d69112db39ae527b11e27862e5837440b
Brad Davidson <brad.davidson@rancher.com>
2026-08-20 08:32:58 +0000

    Convert all backends over to sending unfiltered broadcast watches
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/drivers/t4/backend_smoke_test.go
+++ pkg/drivers/t4/backend_smoke_test.go
@@ -172,7 +172,7 @@ func TestT4Backend_Watch(t *testing.T) {
 		t.Fatalf("CurrentRevision: %v", err)
 	}
 
-	wr := b.Watch(ctx, "/w/", "/w0", startRev+1)
+	wr := b.Watch(ctx, startRev+1)
 
 	doneCh := make(chan struct{})
 	gotCreate, gotUpdate, gotDelete := false, false, false
@@ -230,7 +230,7 @@ func TestT4Backend_CompactAndCompactedWatch(t *testing.T) {
 		t.Fatalf("Compact: %v", err)
 	}
 
-	wr := b.Watch(ctx, "/c/", "/c0", rev1)
+	wr := b.Watch(ctx, rev1)
 	select {
 	case err := <-wr.Errorc:
 		if !errors.Is(err, kserver.ErrCompacted) {
@@ -259,7 +259,7 @@ func TestT4Backend_WatchAtCompactRevision(t *testing.T) {
 
 	// Watching at the compact revision is required to work and to replay the
 	// event recorded at that revision.
-	wr := b.Watch(ctx, "/cw/", "/cw0", rev2)
+	wr := b.Watch(ctx, rev2)
 	select {
 	case err, ok := <-wr.Errorc:
 		t.Fatalf("Watch at compact revision: unexpected error %v (chan open: %v)", err, ok)
